575d2328d740d769a4e8a70d42842bf547c9918d
pyncer <root@pyncer.com>
2024-05-09 03:56:11 -0400

    Fixed bug with getAlias function

--- src/Volume/AbstractVolume.php
+++ src/Volume/AbstractVolume.php
@@ -24,7 +24,7 @@ abstract class AbstractVolume implements VolumeInterface
 
     public function getAlias(): string
     {
-        return $this->getAlias();
+        return $this->alias;
     }
 
     public function initialize(): void
